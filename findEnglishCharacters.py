af69d9381cb3a57f984ac63c2bb95ae2e89b2102
Andrey <beskresnov@gmail.com>
2018-05-23 23:23:34 +0300

    Some changes to make code simpler.

--- findEnglishCharacters.py
+++ findEnglishCharacters.py
@@ -1,9 +1,11 @@
-#Find the set of English characters into the text
+# Find the set of English characters into the text
 def find_english_characters(text):
     english_characters = set()
     for i in text:
-        if (65 <= ord(i) and ord(i) <= 90) or (97 <= ord(i) and ord(i) <= 122):
+        if (65 <= ord(i) <= 90) or (97 <= ord(i) <= 122):
             english_characters.add(i)
 
     return english_characters
+
+
 print(find_english_characters("AAAAAAAAAAAAAAAAAAAAAddsefwerhgykjuk"))
